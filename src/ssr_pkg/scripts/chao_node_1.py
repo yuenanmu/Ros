a9286b0b3c70f61292f1257b8879cc331ed8d119
yuenanmu <1730593502@qq.com>
2026-01-30 11:43:42 +0800

    自定义节点的py节点应用实践

--- src/ssr_pkg/scripts/chao_node_1.py
+++ src/ssr_pkg/scripts/chao_node_1.py
@@ -3,18 +3,20 @@
  
 import rospy
 from std_msgs.msg import String
-
+from qq_msgs.msg import Carry
 if __name__=="__main__":
     rospy.init_node("chao_node")
     rospy.logwarn("chao_node is started ")
 
-    pub=rospy.Publisher("chao_node_toptic",String,queue_size=10)
+    pub=rospy.Publisher("chao_node_toptic",Carry,queue_size=10)
 
     rate=rospy.Rate(1)
 
     while not rospy.is_shutdown():
         rospy.loginfo("chao_node is running")
-        msg=String()
-        msg.data="This is chao_node publishing data"
+        msg=Carry()
+        msg.grade="王者"
+        msg.star=50
+        msg.data="This is chao_node publishing data\n"
         pub.publish(msg)
         rate.sleep()
