3d6b28d7bc96bcde118884c5fa638cc9d418402d
yuenanmu <1730593502@qq.com>
2026-02-26 16:09:43 +0800

    第三版的完美状态机巡航！！

---
+++ src/nav_pkg/src/wp_node_v3.cpp
@@ -0,0 +1,49 @@
+#include<ros/ros.h>
+#include<std_msgs/String.h>
+#include<vector>
+
+ros::Publisher wp_pub;
+std::vector<std::string> waypoints={"1","2","3","4"};
+int wp_index=0;
+
+
+void PublishWayPoint(ros::Publisher& wp_pub){
+    if(wp_index>=(int)waypoints.size()){
+        ROS_INFO("All waypoints have been published.");
+        return;
+    }
+    std_msgs::String msg;
+    msg.data=waypoints[wp_index];
+    wp_pub.publish(msg);
+    ROS_INFO("[PublishWayPoint]: %s", msg.data.c_str());
+}
+void NavResultCallback(const std_msgs::String::ConstPtr& msg)//指针引用
+{
+    ROS_INFO("[NavResultCallback]: %s", msg->data.c_str());
+    if(msg->data=="done"){
+        wp_index++;
+        PublishWayPoint(wp_pub);//发布下一个航点
+    }
+
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "wp_node");
+    ros::NodeHandle nh;
+    wp_pub = nh.advertise<std_msgs::String>("/waterplus/navi_waypoint", 10);
+    ros::Subscriber wp_sub = nh.subscribe("/waterplus/navi_result", 10, &NavResultCallback);
+    // while (ros::ok()) {
+    //     ros::Rate loop_rate(15); // 15 Hz
+        
+    //     ros::spinOnce();
+    //     loop_rate.sleep();
+    // }
+    
+    // [关键修复] 等待连接建立，防止第一条消息丢失
+    ros::Duration(0.5).sleep(); 
+    PublishWayPoint(wp_pub);//发布第一个航点
+    
+    ros::spin();
+    return 0;
+}
